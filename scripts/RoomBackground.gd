de4ccd29aeec7fc725d72541da38607050e12bdd
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2024-04-17 21:10:50 +0100

    progress :)

--- scripts/RoomBackground.gd
+++ scripts/RoomBackground.gd
@@ -9,5 +9,27 @@ func update_background():
 	match Global.current_room:
 		"jillian's room":
 			texture = load("res://sprites/background/jillian_bedroom.png")
+		"abigail's room":
+			texture = load("res://sprites/background/abigail_bedroom.png")
+		"benjamin's room":
+			texture = load("res://sprites/background/benjamin_bedroom.png")
+		"jimothy's room":
+			texture = load("res://sprites/background/jimothy_bedroom.png")
+		"lucas' room":
+			texture = load("res://sprites/background/lucas_bedroom.png")
+		"clara's room":
+			texture = load("res://sprites/background/clara_bedroom.png")
+		"bathroom":
+			texture = load("res://sprites/background/bathroom.png")
+		"upstairs hallway":
+			texture = load("res://sprites/background/upstairs_hallway.png")
+		"downstairs hallway":
+			texture = load("res://sprites/background/downstairs_hallway.png")
+		"kitchen":
+			texture = load("res://sprites/background/kitchen.png")
+		"living room":
+			texture = load("res://sprites/background/living_room.png")
+		"basement":
+			texture = load("res://sprites/background/basement.png")
 		_:
 			texture = load("res://sprites/background/black.png")
